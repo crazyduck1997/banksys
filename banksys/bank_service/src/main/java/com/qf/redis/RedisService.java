70a73b69c1468796ea78c9638cb5fdbb3301d14e
Hoan <1823427924@qq.com>
2019-09-24 15:50:05 +0800

    去掉注解

--- banksys/bank_service/src/main/java/com/qf/redis/RedisService.java
+++ banksys/bank_service/src/main/java/com/qf/redis/RedisService.java
@@ -22,7 +22,6 @@ public class RedisService {
 
 
     //从redis中获取余额
-    //测试
     public Double getBanlance(String key,String cardId){
         Jedis jedis = jedisPool.getResource();
         Double money = null;
